f1b720ece1fb33e3a07d0f713187cec26ef1ea70
DaveS24 <119626708+DaveS24@users.noreply.github.com>
2024-04-30 11:00:32 +0200

    Add strict task formatting and clone repo instead of GitHub API

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,3 +8,14 @@ All notable changes to this project will be documented in this file.
 
 - Filepath fetching through the GitHub API
 - Model-tasks extraction for each filepath
+
+
+## [0.1.1] - 28-04-2024
+
+### Added
+
+- Strict task formatting for each model-task
+
+### Changed
+
+- File fetching by cloning the repository due to GitHub API limitations
--- src/data_parser.py
+++ src/data_parser.py
@@ -14,33 +14,86 @@ def save_task(task_dir, instruction, solution):
     if not os.path.exists(task_dir):
         os.makedirs(task_dir)
 
-    with open(task_dir + "instructions.txt", "a") as i_file:
+    with open(task_dir + "instructions.txt", "a", errors='ignore') as i_file:
         i_file.write(instruction + '\n')
 
-    with open(task_dir + "solutions.txt", "a") as s_file:
+    with open(task_dir + "solutions.txt", "a", errors='ignore') as s_file:
         s_file.write(solution + '\n')
 
 
-def find_matches(pattern, split_char, code, task_dir):
+def filter_invalid(match):
+    """
+    Filter out invalid matches
+
+    :param match:
+    :return:
+    """
+    if len(match) > 500:
+        return None
+
+    if match.count('\n') > 10:
+        return None
+
+    if match.count('{') != match.count('}'):
+        return None
+
+    if match.count('TODO'):
+        return None
+
+    if match.count('DEBUG'):
+        return None
+
+    return match
+
+
+def format_match(match):
+    """
+    Format the provided match
+
+    :param match:
+    :return:
+    """
+    if filter_invalid(match) is None:
+        return None
+
+    comments = re.findall(r'//.*?(\n|$)', match, re.DOTALL)
+    for comment in comments:
+        match = match.replace(comment, '')
+
+    multiline_comments = re.findall(r'/\*.*?\*\*?/', match, re.DOTALL)
+    for comment in multiline_comments:
+        match = match.replace(comment, '')
+
+    match = match.replace('\n', '\\n')
+    match = match.replace('    ', '')
+
+    return match
+
+
+def find_matches(patterns, split_char, code, task_dir):
     """
     Find matches in the provided code and save them as tasks
 
-    :param pattern:
+    :param patterns:
     :param split_char:
     :param code:
     :param task_dir:
     :return:
     """
-    matches = re.findall(pattern, code)
+    for pattern in patterns:
+        matches = re.findall(pattern, code)
 
-    for match in matches:
-        match = match.replace('\n', '\\n')
+        for match in matches:
+            match = format_match(match)
 
-        parts = match.split(split_char)
-        instruction = parts[0] + split_char
-        solution = split_char.join(parts[1:])
+            if match is None:
+                continue
 
-        save_task(task_dir, instruction, solution)
+            parts = match.split(split_char)
+            instruction = parts[0] + split_char
+            solution = split_char.join(parts[1:])
+
+            save_task(task_dir, instruction, solution)
 
 
 def conditional_statement_task(code, data_dir):
@@ -53,8 +106,13 @@ def conditional_statement_task(code, data_dir):
     """
     task_dir = data_dir + "conditional_statement_task/"
 
-    pattern = r'if\s+[^{]*\{[^}]*\}|else\s+if\s+[^{]*\{[^}]*\}|else\s+\{[^}]*\}'
-    find_matches(pattern, '{', code, task_dir)
+    if_pattern = re.compile(r'if\s*\([^{}\n]*?\)\s*\{.*?}', re.DOTALL)
+    else_if_pattern = re.compile(r'else\s+if\s*\([^{}\n]*?\)\s*\{.*?}', re.DOTALL)
+    else_pattern = re.compile(r'else\s*\{.*?}', re.DOTALL)
+    when_pattern = re.compile(r'when\s*\([^{}\n]*?\)\s*\{.*?}', re.DOTALL)
+
+    patterns = [if_pattern, else_if_pattern, else_pattern, when_pattern]
+    find_matches(patterns, '{', code, task_dir)
 
 
 def function_declaration_task(code, data_dir):
@@ -67,8 +125,10 @@ def function_declaration_task(code, data_dir):
     """
     task_dir = data_dir + "function_declaration_task/"
 
-    pattern = r'fun\s+[^{}]*\{[^{}]*\}'
-    find_matches(pattern, '{', code, task_dir)
+    fun_pattern = re.compile(r'fun\s+\w+\s*\([^{}=]*?\)\s*\{.+?}', re.DOTALL)
+
+    patterns = [fun_pattern]
+    find_matches(patterns, '{', code, task_dir)
 
 
 def import_statement_task(code, data_dir):
@@ -81,8 +141,11 @@ def import_statement_task(code, data_dir):
     """
     task_dir = data_dir + "import_statement_task/"
 
-    pattern = r'import\s+[^\n]*'
-    find_matches(pattern, '.', code, task_dir)
+    import_pattern = re.compile(r'import\s+.*?\n')
+    package_pattern = re.compile(r'package\s+.*?\n')
+
+    patterns = [import_pattern, package_pattern]
+    find_matches(patterns, '.', code, task_dir)
 
 
 def loop_statement_task(code, data_dir):
@@ -95,22 +158,11 @@ def loop_statement_task(code, data_dir):
     """
     task_dir = data_dir + "loop_statement_task/"
 
-    pattern = r'for\s+[^{]*\{[^{]*\}|while\s+[^{]*\{[^{]*\}'
-    find_matches(pattern, '{', code, task_dir)
+    for_pattern = re.compile(r'for\s*\([^{}\n]*?\)\s*\{[^{}]*}', re.DOTALL)
+    while_pattern = re.compile(r'while\s*\([^{}\n]*?\)\s*\{[^{}]*}', re.DOTALL)
 
-
-def type_declaration_task(code, data_dir):
-    """
-    Extract type declaration tasks from the provided code file
-
-    :param code:
-    :param data_dir:
-    :return:
-    """
-    task_dir = data_dir + "type_declaration_task/"
-
-    pattern = r'var\s+[^:=]*:[^=]*|val\s+[^:=]*:[^=]*'
-    find_matches(pattern, ':', code, task_dir)
+    patterns = [for_pattern, while_pattern]
+    find_matches(patterns, '{', code, task_dir)
 
 
 def variable_declaration_task(code, data_dir):
@@ -123,8 +175,11 @@ def variable_declaration_task(code, data_dir):
     """
     task_dir = data_dir + "variable_declaration_task/"
 
-    pattern = r'var\s+[^=]*=[^\n]*|val\s+[^=]*=[^\n]*'
-    find_matches(pattern, '=', code, task_dir)
+    var_pattern = re.compile(r'var\s+\w+\s*=\s*.*\n')
+    val_pattern = re.compile(r'val\s+\w+\s*=\s*.*\n')
+
+    patterns = [var_pattern, val_pattern]
+    find_matches(patterns, '=', code, task_dir)
 
 
 def extract_tasks(code):
@@ -134,11 +189,10 @@ def extract_tasks(code):
     :param code:
     :return:
     """
-    data_dir = "../data/"
+    data_dir = "../data/model_tasks/"
 
     conditional_statement_task(code, data_dir)
     function_declaration_task(code, data_dir)
     import_statement_task(code, data_dir)
     loop_statement_task(code, data_dir)
-    type_declaration_task(code, data_dir)
     variable_declaration_task(code, data_dir)
